1e8c008a554e5a030f30138ba32a9d02b4c9d4fa
unknown <vikaemanuely@gmail.com>
2021-11-09 19:53:56 -0300

    Criação das views e serializers

--- animeList/urls.py
+++ animeList/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from core import views
+
+router = routers.DefaultRouter()
+router.register(r'generos-viewset', views.GeneroViewSet)
+router.register(r'estudios-viewset', views.EstudioViewSet)
+# router.register(r'animes-viewset', views.AnimeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('generos/', views.GeneroView.as_view()),
+    path('generos/<int:id>/', views.GeneroView.as_view()),
+    path('generos-apiview/', views.GenerosList.as_view()),
+    path('generos-apiview/<int:id>/', views.GeneroDetail.as_view()),
+    path('generos-generic/', views.GenerosListGeneric.as_view()),
+    path('generos-generic/<int:id>/', views.GeneroDetailGeneric.as_view()),
+    path('', include(router.urls))
 ]
