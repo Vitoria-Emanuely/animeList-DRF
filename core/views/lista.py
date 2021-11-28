e1e10085d7db0fb72cff51b22da75d91392c66a1
Vitória <vikaemanuely@gmail.com>
2021-11-28 02:32:39 -0300

    Filtrando as listas apenas do usuário autenticado

--- core/views/lista.py
+++ core/views/lista.py
@@ -5,9 +5,14 @@ from core.serializers import ListaSerializer, CriarEditarListaSerializer
 
 class ListaViewSet(ModelViewSet):
     queryset = Lista.objects.all()
-    # serializer_class = ListaSerializer
 
     def get_serializer_class(self):
         if self.action == "list" or self.action == "retrieve":
             return ListaSerializer
         return CriarEditarListaSerializer
+
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.groups.filter(name="Administradores"):
+            return Lista.objects.all()
+        return Lista.objects.filter(usuario=usuario)
